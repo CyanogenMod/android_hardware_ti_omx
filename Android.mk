7ca61f6a1c22ba6752e456be08e616be29f6c0b0
ricardo martinez <oscar2357@gmail.com>
2009-02-09 13:07:54 -0600

    Add TI OMX core plugin
    This patch adds the core_plugin to the omx project, this plugin is
    needed in order to register the TI_OMX_Core in the OpenCore framework
    
    Signed-off-by: Ricardo Martinez <oscar2357@gmail.com>

--- Android.mk
+++ Android.mk
@@ -82,4 +82,7 @@ include $(TI_OMX_VIDEO)/video_encode/Android.mk
 include $(TI_OMX_IMAGE)/jpeg_enc/Android.mk
 include $(TI_OMX_IMAGE)/jpeg_dec/Android.mk
 
+#call to plugin
+include $(TI_OMX_TOP)/core_plugin/Android.mk
+
 endif
