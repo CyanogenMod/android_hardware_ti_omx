1c3130bf945181765e63dec71dd2dacd6cccb353
Chris Kelly <c-kelly@ti.com>
2009-04-08 17:16:08 -0500

    OMX AUDIO: Adding G726 Decoder OMX Component
    Seeding the tree with new files for G726 decoder.

--- Android.mk
+++ Android.mk
@@ -85,6 +85,8 @@ include $(TI_OMX_AUDIO)/g722_dec/src/Android.mk
 include $(TI_OMX_AUDIO)/g722_dec/tests/Android.mk
 include $(TI_OMX_AUDIO)/g722_enc/src/Android.mk
 include $(TI_OMX_AUDIO)/g722_enc/tests/Android.mk
+include $(TI_OMX_AUDIO)/g726_dec/src/Android.mk
+include $(TI_OMX_AUDIO)/g726_dec/tests/Android.mk
 
 #call to video
 include $(TI_OMX_VIDEO)/video_decode/Android.mk
