dc046ce5b42ac890b9f20a88d0c460533a09be42
abraham duenas <abraham.duenas@ti.com>
2009-06-04 09:57:58 -0500

    OMX AUDIO: OC 2.04 AAC/MP3 TS unit change
    Change TS unit from milliseconds to microseconds for OC 2.04.
    The scale factor was changed from 1K to 1M for AAC/MP3 decoders.

--- audio/src/openmax_il/mp3_dec/src/OMX_Mp3Dec_Utils.c
+++ audio/src/openmax_il/mp3_dec/src/OMX_Mp3Dec_Utils.c
@@ -2495,7 +2495,7 @@ OMX_ERRORTYPE MP3DEC_LCML_Callback (TUsnCodecEvent event,void * args [10])
                     }else{ 
                         time_stmp = pLcmlHdr->pBufHdr->nFilledLen / (pComponentPrivate->pcmParams->nChannels * 
                                                                      (pComponentPrivate->pcmParams->nBitPerSample / 8));
-                        time_stmp = (time_stmp / pComponentPrivate->pcmParams->nSamplingRate) * 1000;
+                        time_stmp = (time_stmp / pComponentPrivate->pcmParams->nSamplingRate) * 1000000;
                         /* Update time stamp information */
                         pComponentPrivate->temp_TS += time_stmp;
                         pLcmlHdr->pBufHdr->nTimeStamp = pComponentPrivate->temp_TS;
