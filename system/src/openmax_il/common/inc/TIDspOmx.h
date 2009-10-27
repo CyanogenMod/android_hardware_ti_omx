ef77aaaf33d834be200c007883fa4026e7cf53f6
Suneetha Kalahasthi <suneetha.k@ti.com>
2009-10-27 16:14:19 -0600

    Fix for Segmentation fault in G726 Encoder
    Reverting one change in gerrit # 984, TIDspOmx.h, to fix
    segmentation fault in G726 Encoder
    
    Signed-off-by: Suneetha, Kalahasthi <suneetha.k@ti.com>

--- system/src/openmax_il/common/inc/TIDspOmx.h
+++ system/src/openmax_il/common/inc/TIDspOmx.h
@@ -606,7 +606,7 @@
 #define OMX_MMMIXER_ITOA(_alpha_,_integer_) \
 {\
     static char OMX_MMMIXER_ITOA_str[50];\
-    int i = sizeof(OMX_MMMIXER_ITOA_str);\
+    int i = sizeof(OMX_MMMIXER_ITOA_str) - 1;\
     do {\
         OMX_MMMIXER_ITOA_str[--i] = '0' + _integer_ % 10;\
     } while ((_integer_ = _integer_ / 10) && i > 0);\
