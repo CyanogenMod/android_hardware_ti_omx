af4f47b880c2339343791189788adbb82db1c396
Saravanan Solaiyappan <saravanan.s@ti.com>
2009-12-07 15:23:27 -0600

    1. JPEG encoder test support for SR21135; encoding YUV420p images in a single scan by converting it to YUV422i before encoding. 2. OMX_Core.c: dlerror() function call inserted after dlsym() call to reflect the latest error status of the dlsym calls.

--- system/src/openmax_il/omx_core/src/OMX_Core.c
+++ system/src/openmax_il/omx_core/src/OMX_Core.c
@@ -252,6 +252,7 @@ OMX_ERRORTYPE TIOMX_GetHandle( OMX_HANDLETYPE* pHandle, OMX_STRING cComponentNam
                 /* Get a function pointer to the "OMX_ComponentInit" function.  If
                  * there is an error, we can't go on, so set the error code and exit */
                 pComponentInit = dlsym(pModules[i], "OMX_ComponentInit");
+                pErr = dlerror();
                 if( (pErr != NULL) || (pComponentInit == NULL) ) {
                     LOGE("%d:: dlsym failed for module %p\n", __LINE__, pModules[i]);
                     err = OMX_ErrorInvalidComponent;
