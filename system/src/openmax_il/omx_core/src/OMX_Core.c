32c859650fe8d6ffaeb5ae7fa7b9f377c2718d3b
Daniel Charles <dcharles@ti.com>
2010-02-03 13:26:00 -0600

    OMX_Core:  Add 720p H.264/AVC encoder
    This patch is to allow a new omx component to assume the avc role so that it can
    become part of the list of components when encoding 720p.  This patch does not
    contain the 720p h264 enc functionality itself.

--- system/src/openmax_il/omx_core/src/OMX_Core.c
+++ system/src/openmax_il/omx_core/src/OMX_Core.c
@@ -63,6 +63,7 @@ char *tComponentName[MAXCOMP][2] = {
     {"OMX.TI.720P.Encoder", "video_encoder.mpeg4"},
     {"OMX.TI.Video.encoder", "video_encoder.h263"},
     {"OMX.TI.Video.encoder", "video_encoder.avc"},
+    {"OMX.TI.720P.Encoder", "video_encoder.avc"},
     {"OMX.TI.VPP", "iv_renderer.yuv.overlay"},
     {"OMX.TI.Camera", "camera.yuv"},
     
