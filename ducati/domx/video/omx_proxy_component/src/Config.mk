71d3a5443a09d88b8bfd9c193821948ae3727754
Sundar <sunds@ti.com>
2010-09-24 16:37:45 +0530

    DOMX: OMXCamera proxy updates for DCC file loading during OMX_GetHandle
    This patch provides support for OMXCamera proxy to load the DCC files
    from /system/etc/omapcam directory to Ducati as part of OMX_GetHandle
    
    Port of patch from GLMM team for DCC tuning
    
    Change-Id: I3c2539cb3448dd81b96f0e7e9d4e8ffd6a853dbf
    Signed-off-by: Sundar <sunds@ti.com>
    Signed-off-by: Akwasi Boateng <akwasi.boateng@ti.com>

--- ducati/domx/video/omx_proxy_component/src/Config.mk
+++ ducati/domx/video/omx_proxy_component/src/Config.mk
@@ -26,6 +26,7 @@ LOCAL_SHARED_LIBRARIES := \
 		libd2cmap \
 		libomx_proxy_common \
 		libomx_rpc \
+		libcutils \
 
 LOCAL_CFLAGS += -MD -pipe  -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DTMS32060 -D_DB_TIOMAP  -DSYSLINK_USE_SYSMGR -DSYSLINK_USE_LOADER
 LOCAL_CFLAGS += -D_Android
